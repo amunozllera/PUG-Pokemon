344186f3604521b1f6877f437922eced359ed2dd
amunozllera <amunozllera@gmail.com>
2022-09-17 11:28:40 +0200

    try append

--- src/routes/main.public.routes.js
+++ src/routes/main.public.routes.js
@@ -6,13 +6,20 @@ const router = express.Router()
 router.get('/', (req, res) => {
     controller.getAllData()
     .then(data => {
-        res.render('main',{
+        res.render('landing',{
             message: 'PokePUG',
             list: data.list
         }); 
     }).catch((err) => {
         console.log(err)
     });
+});
+
+router.get('/detail', (req, res) => {
+    res.render('detail',{
+        message: 'OTHER VIEW',
+        component:'other'
+    }); 
 });
 
 module.exports = router;
